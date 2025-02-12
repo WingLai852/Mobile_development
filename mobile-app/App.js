3826f301acbf4177b8c1631a67a04fd977bbceb4
Rocky Lai <r0912510@student.thomasmore.be>
2025-02-12 14:02:27 +0100

    Update App.js

--- mobile-app/App.js
+++ mobile-app/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Helloo test test</Text>
+      <Text>Helloo</Text>
       <StatusBar style="auto" />
     </View>
   );
