0945ce6d12c19431dce0dc39269eef1124fddba7
Rocky Lai <r0912510@student.thomasmore.be>
2025-02-17 15:21:30 +0100

    een scrollview toegevoegd

--- mobile-app/App.js
+++ mobile-app/App.js
@@ -5,19 +5,21 @@ import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
 export default function App() {
   return (
+    <ScrollView style={styles.container}>
     <View style={styles.headerContainer}>
-        <Text style={styles.headerTitle}>Welkom bij de Shop</Text>
+        <Text style={styles.headerTitle}>Welkom bij snowboard shop</Text>
         <Text style={styles.headerSubtitle}>Ontdek onze nieuwste producten</Text>
       </View>
-
+      </ScrollView>
   );
+  
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#F5F5F5',
-    paddingTop: 50,
+    paddingTop: 90,
 
   },
   headerContainer: {
