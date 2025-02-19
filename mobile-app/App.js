590505866cd9b4d0bb4c9ffa2af6213cecbe308c
Rocky Lai <r0912510@student.thomasmore.be>
2025-02-19 16:11:59 +0100

    d

--- mobile-app/App.js
+++ mobile-app/App.js
@@ -1,63 +1,20 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
-import ProductCard from './components/ProductCard';
+import React from "react";
+import {NavigationContainer} from "@react-navigation/native";
+import {createStackNavigator} from "@react-navigation/stack";
 
+import HomeScreen from "./screens/HomeScreen.js";
 
+import ProductDetails from "./screens/ProductDetails.js";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    //header section
-    <ScrollView style={styles.container}>
-    <View style={styles.headerContainer}>
-        <Text style={styles.headerTitle}>Welkom bij snowboard shop</Text>
-        <Text style={styles.headerSubtitle}>Ontdek onze nieuwste producten</Text>
-      </View>
-
-      <View style={styles.Productcontainer}>
-        <ProductCard
-        img={require('./assets/snowboard1.jpg')}
-        name="Burton Custom Snowboard"
-        description="500 euro"
-        />
-        <ProductCard
-        img={require('./assets/snowboard1.jpg')}
-        name="Burton Custom Snowboard"
-        description="500 euro"
-        />
-         <ProductCard
-        img={require('./assets/snowboard1.jpg')}
-        name="Burton Custom Snowboard"
-        description="500 euro"
-        />
-        
-        </View>
-      </ScrollView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={ProductDetails} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-  
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F5F5F5',
-    paddingTop: 90,
-
-  },
-  headerContainer: {
-    alignItems: 'center',
-    marginBottom: 20,
-  },
-
-  headerTitle: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: '#333',
-  },
-
-  headerSubtitle: {
-    fontSize: 16,
-    color: '#666',
-  },
-});
-
